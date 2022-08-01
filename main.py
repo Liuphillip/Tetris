40cdfb2418a76d8d0827dfec3da034ddb5c8ba6e
Liuphillip <Liuphillip88@gmail.com>
2022-07-31 22:52:00 -0700

    created window
    created basic window with grey background
    500x500 size

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("hello world")
+import pygame
+
+FPS = 60
+WIDTH = 500
+HEIGHT = 500
+GRID_WIDTH, GRID_HEIGHT = 50, 50
+WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
+
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+GREY = (100, 100, 100)
+
+def draw_window():
+    WINDOW.fill(GREY)
+    pygame.display.update()
+def main():
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        # sets fps
+        clock.tick(FPS)
+
+        # event polling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        # logic
+
+        # Render
+        draw_window()
+
+if __name__ == "__main__":
+    main()
