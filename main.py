61a29a9133274f03a793649c6c317121f8a2a893
Liuphillip <Liuphillip88@gmail.com>
2022-08-02 16:27:34 -0700

    updated main
    decided to restart

--- main.py
+++ main.py
@@ -1,42 +1,31 @@
 import pygame
 
+S_1 = [[0, 0, 0, 0, 0],
+     [],
+     [],
+     [],
+     [0, 0, 0, 0, 0]]
+
 FPS = 60
 WIDTH = 300
 HEIGHT = 600
-BLOCK_WIDTH, BLOCK_HEIGHT = 30, 30
-GRID_WIDTH, GRID_HEIGHT = WIDTH/BLOCK_WIDTH, HEIGHT/BLOCK_HEIGHT
+GRID_BLOCK_SIZE = 30
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-
-GRID_ARRAY = [[0] * int(GRID_WIDTH) for n in range(int(GRID_HEIGHT))]
 
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 GREY = (100, 100, 100)
 
 
-def create_grid(locked_pos = {}):
-    grid = [[(0,0,0) for n in range(GRID_WIDTH)] for n in range(GRID_HEIGHT)]
+# initialize matrix
+# playing space is 10x20
+# there is extra 5 rows at the top for spawning blocks
+# there is extra 1 row at the bottom for detecting collisions
+# total matrix size is 10x26
+matrix = [[0 for n in range(10)] for n in range(25)]
+# adding the extra 1 row to the bottom
+matrix.append([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            if (col, row) in locked_pos:
-                color = locked_pos[(col, row)]
-                grid[col][row] = color
-
-    return grid
-
-
-def grid():
-    x, y = 0, 0
-    for row in GRID_ARRAY:
-        for col in row:
-            block = pygame.Rect(x, y, BLOCK_WIDTH, BLOCK_HEIGHT)
-            pygame.draw.rect(WINDOW, GREEN, block)
-            x += BLOCK_WIDTH
-            # print("x : " + str(x))
-        y += BLOCK_HEIGHT
-        # print("y : " + str(y))
-        x = 0
 
 def draw_window():
     WINDOW.fill(GREY)
